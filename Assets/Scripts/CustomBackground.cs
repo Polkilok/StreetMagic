20dfe483966ef9d4cba6d8be8856c9b30804a10b
Криничный Юрий Алексеевич <polkilok1995@gmail.com>
2017-11-03 13:38:46 +0300

    перешли на последнюю версию Юньки

---
+++ Assets/Scripts/CustomBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBackground : MonoBehaviour {
+
+	public GameObject Background;
+	public GameObject Player;
+	private const float BackgroundDistance = 3f;
+	// Use this for initialization
+	void Start () {
+		//transform.position = Player.transform.position + Player.transform.forward * BackgroundDistance;
+		//transform.LookAt(Player.transform.position + Player.transform.forward * (BackgroundDistance + 1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Player.transform.position + Player.transform.forward * BackgroundDistance;
+		transform.LookAt(Player.transform.position + Player.transform.forward * (BackgroundDistance + 1f));
+	}
+}
